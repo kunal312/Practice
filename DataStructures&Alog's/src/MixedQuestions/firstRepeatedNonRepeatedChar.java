a6a0e4b8f9a9502173475b841f93625b05b3135d
kunal312 <kunal312@hotmail.com>
2017-02-19 19:21:21 -0800

    String (find first repeated and non repeated character)

---
+++ DataStructures&Alog's/src/MixedQuestions/firstRepeatedNonRepeatedChar.java
@@ -0,0 +1,61 @@
+package MixedQuestions;
+
+import java.util.HashMap;
+
+/**
+ * Created by kunal on 2/19/17.
+ */
+
+//Find First Repeated And Non-Repeated Character In A String.
+
+public class firstRepeatedNonRepeatedChar {
+
+
+    public void findrep(String str){
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        char [] ch = str.toCharArray();
+
+        for( char c: ch){
+                if(map.containsKey(c)){
+                    map.put(c,map.get(c)+1);
+                }
+                else{
+                    map.put(c,1);
+                }
+
+        }
+
+        for(char c: ch){
+            if(map.get(c)>1){
+                System.out.println("First Repeatating character "+c);
+                break;
+            }
+        }
+
+
+            for(char c : ch){
+
+
+            if(map.get(c)==1){
+                System.out.println("First Non Repeatating character "+c);
+                break;
+            }
+        }
+
+
+
+    }
+
+    public static void main(String[] args){
+
+        firstRepeatedNonRepeatedChar r = new firstRepeatedNonRepeatedChar();
+        r.findrep("ybccbiiaagagagaijogaoxgag");
+
+
+
+    }
+
+
+}
